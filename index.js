5886e8635adbb06d1c46bffde87f72216885757f
Claire Sersun <claire.sersun@gmail.com>
2022-09-04 15:16:19 -0400

    remove errors

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("fs/promises");
 const chalk = require("chalk");
 const { writeFile } = require("fs");
 const QUOTE_FILE = "./quotes.txt";
-const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
 
 program
   .name("quotes")
@@ -14,54 +13,27 @@ program
   .command("getQuote")
   .description("Retrieves a random quote")
   .action(async () => {
-    // TODO: Pull a random quote from the quotes.txt file
-      const data = await fs.readFile(QUOTE_FILE, 'utf-8', function(err){
-      if (err) throw err})
-      // choose one of the lines...
-      // const line = Math.floor(Math.random()*data.length)
-      const fileBuffer =  data
-      const to_string = fileBuffer.toString();
-      const split_lines = to_string.split("\n");
-      const randomLine = Math.floor(Math.random()*split_lines.length)-1
-      const selectedLine = split_lines[randomLine]
-      const quoteAndAuthor = selectedLine.split('|')
-      const quote = JSON.stringify(quoteAndAuthor[0])
-      const author = JSON.stringify(quoteAndAuthor[1])
-      // // You may style the text with chalk as you wish
-      const colorQuote = chalk.bgCyan(quote)
-      const colorAuthor = chalk.cyan(author)
-      console.log(colorQuote)
-      console.log(colorAuthor)
-      // // console log the quote and author
-      // console.log(colorQuote)
-      // console.log(colorAuthor)
-      // pipe(console.log)(colorQuote, colorAuthor)
-      // const fileContents = await fs.readFile(QUOTE_FILE, 'utf-8')
-      // console.log(fileContents[0])
+      const data = await fs.readFile(QUOTE_FILE, "utf-8");
+      const split_lines = data.split("\n").filter((line) => line);
+      const randomLine = Math.floor(Math.random() * split_lines.length);
+      const selectedLine = split_lines[randomLine];
+      const quoteAndAuthor = selectedLine.split("|");
+      const quote = quoteAndAuthor[0];
+      const author = quoteAndAuthor[1];
+      const colorQuote = chalk.bgCyan(quote);
+      const colorAuthor = chalk.cyan(author);
+      console.log(colorQuote);
+      console.log(colorAuthor);
     });
 
 program
   .command("addQuote <quote> [author]")
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
-    // TODO: Add the quote and author to the quotes.txt file
-    // If no author is provided,
-    // After the quote/author is saved,
-    // alert the user that the quote was added.
-    const colorTxt = chalk.yellow('Your quote was saved!')
-    // You may style the text with chalk as you wish
-    // HINT: You can store both author and quote on the same line using
-    // a separator like pipe | and then using .split() when retrieving
-    const newLineQuote = '\n' + quote
-    const addQuote = await fs.appendFile(QUOTE_FILE, newLineQuote, function(err){
-      if (err) throw err
-    })
-    const separatorAuthor = ' |' + author
-    const addAuthor = await fs.appendFile(QUOTE_FILE, separatorAuthor, function(err){
-      if (err) author = 'Anonymous'
-    })
-    pipe(addQuote, addAuthor)
-    console.log(colorTxt)
+    const newLineQuote = "\n" + quote + "|" + (author ? author : "Anonymous");
+    await fs.appendFile(QUOTE_FILE, newLineQuote);
+    const colorTxt = chalk.yellow('Your quote was saved!');
+    console.log(colorTxt);
   });
 
 program.parse();
